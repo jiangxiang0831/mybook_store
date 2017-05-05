979678e41abf88c2311b39b69606c671bcbc3db9
a0912124179 <a0912124179@gmail.com>
2017-05-05 03:09:05 -0400

    finish list book feature

---
+++ db/migrate/20170505070346_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.text :introduction
+      t.string :authorname
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
